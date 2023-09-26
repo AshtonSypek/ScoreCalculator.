09c2aebc59495a072a3f475283b9ae731747c025
AshtonSypek <145035312+AshtonSypek@users.noreply.github.com>
2023-09-26 06:44:51 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.Write("Enter your age: ");
+    string userInput = Console.ReadLine();
+    int age;
+
+    if (int.TryParse(userInput, out age))
+    {
+        if (age >= 18)
+        {
+            Console.WriteLine("You can vote!");
+        }
+        else
+        {
+            Console.WriteLine("You can't vote!");
+        }
+    }
+    else
+    {
+        Console.WriteLine(value: "Number given is not received as a real number");
+    }
+}
